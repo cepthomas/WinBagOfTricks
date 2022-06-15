d3e7d5d7fee7ebec0978a3c2e996c393992dd807
cepthomas <cepthomas@gmail.com>
2022-06-15 14:37:29 -0500

    WIP.

--- ClipboardEx/ClipDisplay.cs
+++ ClipboardEx/ClipDisplay.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Runtime.InteropServices;
 using System.Drawing.Printing;
 using NBagOfTricks;
 using NBagOfUis;
